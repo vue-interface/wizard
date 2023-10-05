c171ce055d44cb26f94f118689412604b0c87b09
Mitchell Solomon <msolomonqwerty@gmail.com>
2023-10-05 12:29:44 -0400

    fix: fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,36 +2,40 @@ import vue from '@vitejs/plugin-vue';
 import { pascalCase } from 'change-case';
 import path from 'path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
-import { name } from './package.json';
+import pkg from './package.json';
 
-const fileName = name.split('/')[1];
+const fileName = pkg.name.split('/')[1];
 
-export default defineConfig({
+const external = [
+    ...(pkg.dependencies ? Object.keys(pkg.dependencies) : []),
+    ...(pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : [])
+];
+
+export default ({ command }) => defineConfig({
     build: {
-        sourcemap: process.env.NODE_ENV === 'production',
+        sourcemap: command === 'build',
         lib: {
             entry: path.resolve(__dirname, 'index.ts'),
             name: pascalCase(fileName),
             fileName,
         },
         rollupOptions: {
-            external: ['vue'],
+            external,
             output: {
-                globals: {
-                    vue: 'Vue'
-                },
+                globals: external.reduce((carry, dep) => {
+                    return Object.assign(carry, {
+                        [dep]: pascalCase(dep)
+                    });
+                }, {}),
             }
         },
         watch: !process.env.NODE_ENV && {
             include: [
-                './tailwindcss.js',
                 './tailwindcss/**/*.js'
             ]
         }
     },
     plugins: [
-        vue(),
-        dts()
+        vue()
     ],
 });
