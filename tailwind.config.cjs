40ed03bb6b25ff052d96f485edab3f5cd0a603c0
Mitchell Solomon <msolomonqwerty@gmail.com>
2023-10-05 12:26:39 -0400

    fix: tailwind config issues

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,10 +6,12 @@ module.exports = {
     theme: {
         extend: {},
     },
+    presets: [
+        require('@vue-interface/form-control/tailwindcss')
+    ],
     plugins: [
-        require('@vue-interface/btn/tailwindcss'),
+        require('@vue-interface/btn/tailwindcss')(),
         require('@vue-interface/btn-group/tailwindcss'),
-        ...require('@vue-interface/form-control/tailwindcss'),
     ],
     safelist: [
         ...require('@vue-interface/btn/tailwindcss/safelist')(),
